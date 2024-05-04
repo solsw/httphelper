5463553eba5ede0ca834a01b51ca2731582a09b4
Oleg Subachev <git@solsw.ru>
2024-05-04 22:23:10 +0500

    JsonBody added

--- httphelper.go
+++ httphelper.go
@@ -1,8 +1,26 @@
 package httphelper
 
 import (
+	"bytes"
 	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
 )
+
+// JsonBody sets [http.Request]'s body to JSON-encoded 'in', if not nil.
+func JsonBody(rq *http.Request, in any) (*http.Request, error) {
+	if in == nil {
+		return nil, errors.New("nil input")
+	}
+	jin, err := json.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+	rq.Body = io.NopCloser(bytes.NewReader(jin))
+	return rq, nil
+}
 
 // AuthBasic returns Basic authorization value.
 func AuthBasic(userid, password string) string {
--- rest/rest.go
+++ rest/rest.go
@@ -15,43 +15,43 @@ import (
 // and returns the contents of the response body.
 //
 // If 'isError' is not nil and returns 'true', [httphelper.Error] is returned.
-// [httphelper.Error.Object] of type 'E' is then JSON-decoded from the response body.
+// [httphelper.Error.Object] of type 'E' is JSON-decoded from the response body.
 // Pass [generichelper.NoType] as 'E' to omit processing of [httphelper.Error.Object].
 func BodyBody[E any](ctx context.Context, client *http.Client, method, url string,
 	header http.Header, in io.Reader, isError func(*http.Response) bool) ([]byte, error) {
-	req, err := http.NewRequestWithContext(ctx, method, url, in)
+	rq, err := http.NewRequestWithContext(ctx, method, url, in)
 	if err != nil {
 		return nil, err
 	}
-	req.Header = header
-	resp, err := client.Do(req)
+	rq.Header = header
+	rs, err := client.Do(rq)
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
-	if isError != nil && isError(resp) {
+	defer rs.Body.Close()
+	if isError != nil && isError(rs) {
 		// !generichelper.IsNoType[E]() - is checked in NewError
-		herr, err := httphelper.NewError[E](resp, httphelper.ErrorOptionWithObject())
+		herr, err := httphelper.NewError[E](rs, httphelper.ErrorOptionWithObject())
 		if err != nil {
 			return nil, err
 		}
 		return nil, herr
 	}
-	return io.ReadAll(resp.Body)
+	return io.ReadAll(rs.Body)
 }
 
 // BodyJson performs REST request-response sequence with 'in' request body
-// and output object of type 'O' passed JSON-encoded as the response body.
+// and returns output object of type 'O' passed JSON-encoded as the response body.
 //
 // If 'isError' is not nil and returns 'true', [httphelper.Error] is returned.
-// [httphelper.Error.Object] of type 'E' is then JSON-decoded from the response body.
+// [httphelper.Error.Object] of type 'E' is JSON-decoded from the response body.
 //
 // Pass [generichelper.NoType] as corresponding [type argument] to omit processing of either object.
 //
 // [type argument]: https://go.dev/ref/spec#Instantiations
 func BodyJson[O, E any](ctx context.Context, client *http.Client, method, url string,
 	header http.Header, in io.Reader, isError func(*http.Response) bool) (*O, error) {
-	bbOut, err := BodyBody[E](ctx, client, method, url, header, in, isError)
+	bbout, err := BodyBody[E](ctx, client, method, url, header, in, isError)
 	if err != nil {
 		return nil, err
 	}
@@ -59,7 +59,7 @@ func BodyJson[O, E any](ctx context.Context, client *http.Client, method, url st
 		return nil, nil
 	}
 	var out O
-	if err := json.Unmarshal(bbOut, &out); err != nil {
+	if err := json.Unmarshal(bbout, &out); err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -70,7 +70,7 @@ func BodyJson[O, E any](ctx context.Context, client *http.Client, method, url st
 // 'I' - type of the input object. 'O' - type of the output object.
 //
 // If 'isError' is not nil and returns 'true', [httphelper.Error] is returned.
-// [httphelper.Error.Object] of type 'E' is then JSON-decoded from the response body.
+// [httphelper.Error.Object] of type 'E' is JSON-decoded from the response body.
 //
 // Pass [generichelper.NoType] as corresponding [type argument] to omit processing of either object.
 //
@@ -79,11 +79,57 @@ func JsonJson[I, O, E any](ctx context.Context, client *http.Client, method, url
 	header http.Header, in *I, isError func(*http.Response) bool) (*O, error) {
 	var body io.Reader
 	if in != nil && !generichelper.IsNoType[I]() {
-		bbIn, err := json.Marshal(in)
+		jin, err := json.Marshal(in)
 		if err != nil {
 			return nil, err
 		}
-		body = bytes.NewReader(bbIn)
+		body = bytes.NewReader(jin)
 	}
 	return BodyJson[O, E](ctx, client, method, url, header, body, isError)
+}
+
+// ReqBody sends an [http.Request] by [http.Client] and returns the contents of the response body.
+//
+// If 'isError' is not nil and returns 'true', [httphelper.Error] is returned.
+// [httphelper.Error.Object] of type 'E' is JSON-decoded from the response body.
+// Pass [generichelper.NoType] as 'E' to omit processing of [httphelper.Error.Object].
+func ReqBody[E any](client *http.Client, rq *http.Request, isError func(*http.Response) bool) ([]byte, error) {
+	rs, err := client.Do(rq)
+	if err != nil {
+		return nil, err
+	}
+	defer rs.Body.Close()
+	if isError != nil && isError(rs) {
+		// !generichelper.IsNoType[E]() - is checked in NewError
+		herr, err := httphelper.NewError[E](rs, httphelper.ErrorOptionWithObject())
+		if err != nil {
+			return nil, err
+		}
+		return nil, herr
+	}
+	return io.ReadAll(rs.Body)
+}
+
+// ReqJson sends an [http.Request] by [http.Client] and returns
+// output object of type 'O' passed JSON-encoded as the response body.
+//
+// If 'isError' is not nil and returns 'true', [httphelper.Error] is returned.
+// [httphelper.Error.Object] of type 'E' is JSON-decoded from the response body.
+//
+// Pass [generichelper.NoType] as corresponding [type argument] to omit processing of either object.
+//
+// [type argument]: https://go.dev/ref/spec#Instantiations
+func ReqJson[O, E any](client *http.Client, rq *http.Request, isError func(*http.Response) bool) (*O, error) {
+	bbout, err := ReqBody[E](client, rq, isError)
+	if err != nil {
+		return nil, err
+	}
+	if generichelper.IsNoType[O]() {
+		return nil, nil
+	}
+	var out O
+	if err := json.Unmarshal(bbout, &out); err != nil {
+		return nil, err
+	}
+	return &out, nil
 }
