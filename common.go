b50a7e7b4561fab2cdd14c1895954ddfbcba0b11
Oleg Subachev <git@solsw.ru>
2025-03-31 15:14:08 +0500

    IsNotStatus2xx added

--- common.go
+++ common.go
@@ -7,7 +7,12 @@ import (
 
 var ErrEmptyResponseBody = errors.New("empty response body")
 
-// IsNotStatusOK determines whether r.StatusCode is not [http.StatusOK].
+// IsNotStatusOK determines whether rs.StatusCode is not [http.StatusOK].
 func IsNotStatusOK(rs *http.Response) bool {
 	return rs.StatusCode != http.StatusOK
+}
+
+// IsNotStatus2xx determines whether rs.StatusCode is not a 2xx succesful one.
+func IsNotStatus2xx(rs *http.Response) bool {
+	return !(http.StatusOK <= rs.StatusCode && rs.StatusCode < http.StatusMultipleChoices)
 }
